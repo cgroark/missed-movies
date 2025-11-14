8fb1d33fc2fb73ec9a1e64bd35e3b56c38feace0
Colin Groark <colin@nexleaf.org>
2025-11-14 16:09:36 +0000

    debug backend

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import serverless from "serverless-http";
 import app from "../backend/src/index";
+console.log("Serverless function loaded api dir");
 
 export default serverless(app);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,19 +4,25 @@ import dotenv from 'dotenv';
 import movieRouter from './routes/movies';
 import categoryRouter from './routes/categories';
 
-dotenv.config();
+dotenv.config?.();
+console.log("Serverless function loaded backend src 8");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+console.log("Serverless function loaded backend src 13");
 
 app.use('/movies', movieRouter);
 app.use('/categories', categoryRouter);
+console.log("Serverless function loaded backend src 17");
+
 
 const PORT = process.env.PORT || 4000;
 if (process.env.NODE_ENV !== 'production') {
   console.log('reached dev port')
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
+console.log("Serverless function loaded backend src 25");
+
 
 export default app;
