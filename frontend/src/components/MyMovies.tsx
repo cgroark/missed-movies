0140a89618aa45166298f6124d4811e15cc70662
Colin Groark <colin@nexleaf.org>
2025-11-21 11:02:46 +0000

    lint fix

--- frontend/src/components/MyMovies.tsx
+++ frontend/src/components/MyMovies.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link, useSearchParams } from 'react-router-dom';
 import { FilmStripIcon, FunnelIcon, NotePencilIcon } from '@phosphor-icons/react';
