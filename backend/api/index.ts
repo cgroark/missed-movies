c91622d748e3fc80fe89004a6b553c7281ed1dfe
Colin Groark <colin@nexleaf.org>
2025-11-22 20:23:41 +0000

    backend index path update

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import movieRouter from './movies';
-import categoryRouter from './categories';
+import movieRouter from '../src/routes/movies';
+import categoryRouter from '../src/routes/categories';
 import serverless from 'serverless-http';
 
 dotenv.config();
@@ -11,8 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/movies', movieRouter);
-app.use('/categories', categoryRouter);
+app.use('/api/movies', movieRouter);
+app.use('/api/categories', categoryRouter);
 
 if (process.env.VERCEL !== '1') {
   const PORT = process.env.PORT || 4000;
